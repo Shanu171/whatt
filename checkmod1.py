26fe60dbc456c99dc19034390ed0a9053d327552
Shanu171 <45221019+Shanu171@users.noreply.github.com>
2022-07-29 12:52:41 +0530

    Add files via upload

---
+++ checkmod1.py
@@ -0,0 +1,16 @@
+def ceil(n):
+    e = n ** 2
+    return e
+
+def pront():
+    r = 'hello'
+    print(r)
+    return r
+
+def sub(x, y):
+    c = y - x
+    return c
+
+hi = 23
+
+d = {'Name':'Harshil', 'Age':30}
